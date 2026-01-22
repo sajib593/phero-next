b4cde3f7bf040feade80439eda9e1e62064b70b7
sajib593 <sajib.hoossain11@gmail.com>
2026-01-21 23:04:00 -0800

    change backend

---
+++ app/api/items/route.js
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+
+const items = [
+  {
+    id: 1,
+    name: "Laptop",
+    description: "High performance laptop",
+    price: 1200,
+    image: "https://smartbd.com/wp-content/uploads/2024/07/HP-14-ep0204TU-Laptop.jpg"
+  },
+  {
+    id: 2,
+    name: "Phone",
+    description: "Smartphone with great camera",
+    price: 700,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5FyhKFiRoCdmrxbS5CDkACBhuak_CQjnhKg&s"
+  },
+  {
+    id: 3,
+    name: "Headphone",
+    description: "Noise cancelling headphone",
+    price: 200,
+    image: "https://rukminim2.flixcart.com/fk-p-flap/824/586/image/cc12b8a4642a606f.jpg?q=90"
+  }
+];
+
+// GET all items
+export async function GET() {
+  return NextResponse.json(items);
+}
